dd02696cf97ee242242559d41f58b508bcd3c045
pandaclark <hcy.zju@gmail.com>
2019-07-24 23:43:00 +0800

    完善管理员用户页面

--- src/pages/admin/CentralBankPage.js
+++ src/pages/admin/CentralBankPage.js
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
 import {makeStyles} from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar/Avatar";
+import {GlobalContext} from "../../hooks/GlobalContext";
 
 
 const useStyles = makeStyles(theme => ({
@@ -17,13 +21,59 @@ const useStyles = makeStyles(theme => ({
     showData: {
         flexGrow: 1,
         textAlign: "center",
+    },
+    content: {
+        padding: '16px',
+        margin: '20px',
+        width: '30vw'
     }
 }));
 
 export default function CentralBank() {
+
+    const classes = useStyles();
+    const { state, dispatch } = useContext(GlobalContext);
+    const [ recharge, setRecharge ] = useState(false);
+    const [ transfer, setTransfer ] = useState(false);
+
     return (
+        <div className={classes.root}>
+            <Paper className={classes.paper} component='div' elevation={5} >
+                <Typography variant='h4' paragraph align='center'>
+                    {/*{state.isLogin && <div>Hello {state.username}!</div>} */}
+                    查看中央账户！
+                </Typography>
+                <div style={{padding: '16px'}}>
+                    <div style={{display: "flex", alignItems: "center"}}>
+                        <Avatar className={classes.avatar} src="https://material-ui.com/static/images/avatar/1.jpg"/>
+                        <div style={{flexGrow: 1}}>{/*为了方便布局，加入的空标签*/}</div>
+                        <div style={{flexGrow: 4}}>
+                            <Typography variant="h5">{state.userType} hello {state.username}</Typography>
+                            <Typography variant="subtitle2" color="textSecondary">September 14, 2016</Typography>
+                        </div>
+                    </div>
+                    <hr style={{margin: 20}}/>
+                </div>
+                <SimpleForm/>
+            </Paper>
+        </div>
+    )
+}
+
+function SimpleForm() {
+    const classes = useStyles();
+    return(
         <div>
-            查看中央账户界面
+            <Paper className={classes.content}>
+                <div style={{margin: '16px'}}>
+                    <Typography variant="h6">中央账户：centralbank@email.com</Typography>
+                    <hr/>
+                </div>
+                <div style={{marginLeft: '16px'}}>
+                    <Typography variant="subtitle1" color="textSecondary">账户余额：0.183</Typography>
+                    <Typography variant="subtitle1" color="textSecondary">总共发行课程币数量：150</Typography>
+                </div>
+            </Paper>
         </div>
     )
 }
--- src/pages/admin/ContrastPage.js
+++ src/pages/admin/ContrastPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {makeStyles} from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 
 const useStyles = makeStyles(theme => ({
@@ -17,13 +19,54 @@ const useStyles = makeStyles(theme => ({
     showData: {
         flexGrow: 1,
         textAlign: "center",
+    },
+    content: {
+        padding: '16px',
+        margin: '20px',
+        width: '30vw'
     }
 }));
 
 export default function Contrast() {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            权益分配合约查看界面
+        <div className={classes.root}>
+            <Paper className={classes.paper} component='div' elevation={5} >
+                <br/>
+                <Typography variant='h4' align='center'>
+                    {/*{state.isLogin && <div>Hello {state.username}!</div>} */}
+                    权益分配合约
+                </Typography>
+                <div style={{padding: '16px'}}>
+                    {/*<div style={{display: "flex", alignItems: "center"}}>*/}
+                        {/*/!*<Avatar className={classes.avatar} src="https://material-ui.com/static/images/avatar/1.jpg"/>*!/*/}
+                        {/*<div style={{flexGrow: 1}}>/!*为了方便布局，加入的空标签*!/</div>*/}
+                        {/*<div style={{flexGrow: 4}}>*/}
+                            {/*<Typography variant="h5">hello</Typography>*/}
+                            {/*<Typography variant="subtitle2" color="textSecondary">September 14, 2016</Typography>*/}
+                        {/*</div>*/}
+                    {/*</div>*/}
+                    <hr style={{margin: 20}}/>
+                </div>
+                <ContrastTable/>
+            </Paper>
+        </div>
+    )
+}
+
+function ContrastTable(props) {
+    return(
+        <div style={{display: 'flex'}}>
+            <div style={{flexGrow: 1}}>
+                <Typography variant='h6'>平台</Typography>
+                <Typography variant='body1'>0.03%</Typography>
+            </div>
+            <div style={{flexGrow: 1}}>
+                <Typography variant='h6'>服务提供商</Typography>
+                <Typography variant='body1'>99.7%</Typography>
+            </div>
         </div>
     )
 }
